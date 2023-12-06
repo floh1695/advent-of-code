3bb4634d570722f13899484831310770bb9e1cfc
Charles Bayley <cbayley95@gmail.com>
2023-12-05 23:56:54 -0500

    Framed day 2

--- solutions/app/Main.hs
+++ solutions/app/Main.hs
@@ -1,15 +1,20 @@
 module Main (main) where
 
 import qualified Solutions.Year2023.Day1 as Day1
+import qualified Solutions.Year2023.Day2 as Day2
 
 main :: IO ()
 main = do
   putStrLn "Solutions:"
   putStrLn ""
 
-  day1Input <- readFile "inputs/2023/day1/input.txt"
-  printSolution "2023 Day 1 Problem 1" $ Day1.problem1 day1Input
-  printSolution "2023 Day 1 Problem 2" $ Day1.problem2 day1Input
+  inputDay1 <- readFile "inputs/2023/day1/input.txt"
+  printSolution "2023 Day 1 Problem 1" $ Day1.problem1 inputDay1
+  printSolution "2023 Day 1 Problem 2" $ Day1.problem2 inputDay1
+
+  inputDay2 <- readFile "inputs/2023/day2/input.txt"
+  printSolution "2023 Day 2 Problem 1" $ Day2.problem1 inputDay2
+  printSolution "2023 Day 2 Problem 2" $ Day2.problem2 inputDay2
 
   putStrLn "End Solutions"
 
---
+++ solutions/src/Solutions/Year2023/Day2.hs
@@ -0,0 +1,14 @@
+module Solutions.Year2023.Day2
+  ( problem1
+  , problem2
+  ) where
+
+problem1 :: String -> Int
+problem1 input = solution
+  where
+    solution = 0
+
+problem2 :: String -> Int
+problem2 input = solution
+  where
+    solution = 0
