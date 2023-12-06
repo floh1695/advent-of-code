2d318beea9a7ca05366ddd8a735bb6b7aba5daf2
Charles Bayley <cbayley95@gmail.com>
2023-12-05 20:38:58 -0500

    Solves day1 problem2

--- solutions/app/Main.hs
+++ solutions/app/Main.hs
@@ -4,12 +4,17 @@ import qualified Solutions.Year2023.Day1 as Day1
 
 main :: IO ()
 main = do
-  printSolution "2023 Day 1 Problem 1" Day1.doProblem1
+  putStrLn "Solutions:"
+  putStrLn ""
 
-printSolution :: Show a => String -> IO a -> IO ()
-printSolution title solutionAction = do
-  solution <- solutionAction
+  day1Input <- readFile "inputs/2023/day1/input.txt"
+  printSolution "2023 Day 1 Problem 1" $ Day1.problem1 day1Input
+  printSolution "2023 Day 1 Problem 2" $ Day1.problem2 day1Input
 
+  putStrLn "End Solutions"
+
+printSolution :: Show a => String -> a -> IO ()
+printSolution title solution = do
   putStrLn $ "Problem: " ++ title
   putStrLn $ "solution: " ++ show solution
   putStrLn ""
--- solutions/src/Solutions/Year2023/Day1.hs
+++ solutions/src/Solutions/Year2023/Day1.hs
@@ -1,14 +1,14 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use infix" #-}
+
 module Solutions.Year2023.Day1
-  ( doProblem1
-  , problem1
+  ( problem1
+  , problem2
   ) where
 
+import qualified Data.Bifunctor as Bifunctor
 import qualified Data.Char as Char
-
-doProblem1 :: IO Int
-doProblem1 = do
-  input <- readFile "inputs/2023/day1/input.txt"
-  return $ problem1 input
+import qualified Data.List as List
 
 problem1 :: String -> Int
 problem1 input = solution
@@ -17,3 +17,63 @@ problem1 input = solution
     digits   = map (filter Char.isDigit) inputs
     numbers  = map (\x -> read [head x, last x]) digits
     solution = sum numbers
+
+problem2 :: String -> Int
+problem2 input = solution
+  where
+    inputs   = lines input
+    go :: [String] -> [String] -> String
+    go taken []               = unlines . reverse $ taken
+    go taken (check:checking) = go (check' : taken) checking
+      where
+        check' = ensureDigits check
+    input'   = go [] inputs
+    solution = problem1 input'
+
+ensureDigits :: String -> String
+ensureDigits = ensureLastDigit . ensureFirstDigit
+
+ensureFirstDigit :: String -> String
+ensureFirstDigit = ensureDigit wordDigitPairs
+
+ensureLastDigit :: String -> String
+ensureLastDigit = reverse . ensureDigit (map (Bifunctor.first reverse) wordDigitPairs) . reverse
+
+ensureDigit :: [(String, Char)] -> String -> String
+ensureDigit wordDigits input = result
+  where
+    go :: String -> String -> String
+    go taken "" = taken
+    go taken checking
+      | Char.isDigit . head $ checking  =  reverse taken ++ checking
+      | checking /= checking'           =  reverse taken ++ checking'
+      | otherwise                       =  go (head checking' : taken) (tail checking')
+      where
+        checking' = replaceNextWordWithDigit wordDigits checking
+    result = go "" input
+
+replaceNextWordWithDigit :: [(String, Char)] -> String -> String
+replaceNextWordWithDigit wordDigits input = replaced
+  where
+    replacers = map (uncurry replaceNextWordWithDigit') wordDigits
+    replacer  = foldl (.) id replacers
+    replaced  = replacer input
+
+replaceNextWordWithDigit' :: String -> Char -> String -> String
+replaceNextWordWithDigit' word digit input =
+  if List.isPrefixOf word input
+  then digit : drop (length word) input
+  else input
+
+wordDigitPairs :: [(String, Char)]
+wordDigitPairs =
+  [ ("one",   '1')
+  , ("two",   '2')
+  , ("three", '3')
+  , ("four",  '4')
+  , ("five",  '5')
+  , ("six",   '6')
+  , ("seven", '7')
+  , ("eight", '8')
+  , ("nine",  '9')
+  ]
