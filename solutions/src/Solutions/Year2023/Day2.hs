1bf2c91ca043079e9e2359772bb9d960cf45add4
Charles Bayley <cbayley95@gmail.com>
2023-12-06 03:24:58 -0500

    Solve day 2 problem 1

--- solutions/src/Solutions/Year2023/Day2.hs
+++ solutions/src/Solutions/Year2023/Day2.hs
@@ -3,12 +3,93 @@ module Solutions.Year2023.Day2
   , problem2
   ) where
 
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+
 problem1 :: String -> Int
 problem1 input = solution
   where
-    solution = 0
+    games       = parseGames input
+    test        = newHand 12 13 14
+    passing     = filter (testGame test) games
+    identifiers = map identifier passing
+    solution    = sum identifiers
 
 problem2 :: String -> Int
 problem2 input = solution
   where
     solution = 0
+
+data Game = Game
+  { identifier :: Int
+  , hands      :: [Hand]
+  }
+  deriving (Show)
+
+newGame :: Int -> [Hand] -> Game
+newGame identifier' hands' = Game
+  { identifier = identifier'
+  , hands      = hands'
+  }
+
+parseGame :: String -> Game
+parseGame input = newGame (read identifier') hands'
+  where
+    atIdentifier = dropWhile (not . Char.isDigit) input
+    identifier'  = takeWhile Char.isDigit atIdentifier
+    atHands      = drop (length identifier' + length ": ") atIdentifier
+    hands'       = map parseHand . Split.splitOn "; " $ atHands
+
+parseGames :: String -> [Game]
+parseGames input = map parseGame $ lines input
+
+testGame :: Hand -> Game -> Bool
+testGame test game = all (testHand test) $ hands game
+
+data Hand = Hand
+  { red   :: Int
+  , green :: Int
+  , blue  :: Int
+  }
+  deriving (Show)
+
+newHand :: Int -> Int -> Int -> Hand
+newHand red' green' blue' = Hand
+  { red   = red'
+  , green = green'
+  , blue  = blue'
+  }
+
+parseHand :: String -> Hand
+parseHand input = newHand red' green' blue'
+  where
+    cubeses    = map parseCubes . Split.splitOn ", " $ input
+    forColor c = maybe 0 count . List.find ((== c) . color) $ cubeses
+    red'       = forColor "red"
+    green'     = forColor "green"
+    blue'      = forColor "blue"
+
+handCount :: Hand -> Int
+handCount hand = sum $ map ($ hand) [red, green, blue]
+
+testHand :: Hand -> Hand -> Bool
+testHand test hand = all (\f -> f test >= f hand) [handCount, red, green, blue]
+
+data Cubes = Cubes
+  { color :: String
+  , count :: Int
+  }
+
+newCubes :: String -> Int -> Cubes
+newCubes color' count' = Cubes
+  { color = color'
+  , count = count'
+  }
+
+parseCubes :: String -> Cubes
+parseCubes input = newCubes color' count'
+  where
+    tokens  = Split.splitOn " " input
+    count'  = read $ head tokens
+    color'  = last tokens
