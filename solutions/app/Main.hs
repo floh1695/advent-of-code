ada7fa025fb2ab9f842a18b6307f0127cf18fbca
Charles Bayley <cbayley95@gmail.com>
2023-12-06 23:58:16 -0500

    Better structure for calling solutions

--- solutions/app/Main.hs
+++ solutions/app/Main.hs
@@ -8,15 +8,17 @@ main = do
   putStrLn "Solutions:"
   putStrLn ""
 
-  inputDay1 <- readFile "inputs/2023/day1/input.txt"
-  printSolution "2023 Day 1 Problem 1" $ Day1.problem1 inputDay1
-  printSolution "2023 Day 1 Problem 2" $ Day1.problem2 inputDay1
-
-  inputDay2 <- readFile "inputs/2023/day2/input.txt"
-  printSolution "2023 Day 2 Problem 1" $ Day2.problem1 inputDay2
-  printSolution "2023 Day 2 Problem 2" $ Day2.problem2 inputDay2
+  doDay 2023 1 Day1.problem1 Day1.problem2
+  doDay 2023 2 Day2.problem1 Day2.problem2
 
   putStrLn "End Solutions"
+
+doDay :: Show a => Int -> Int -> (String -> a) -> (String -> a) -> IO ()
+doDay year day problem1 problem2 = do
+  let title = unwords ["Year", show year, "Day", show day]
+  input <- readFile $ mconcat ["inputs/year", show year, "/day", show day, "/input.txt"]
+  printSolution (title ++ " Problem 1") $ problem1 input
+  printSolution (title ++ " Problem 2") $ problem2 input
 
 printSolution :: Show a => String -> a -> IO ()
 printSolution title solution = do
